2e29f6a685ac4c088b48e7f6f0d008c1db7208e5
jiashu yang <yangjs1998@gmail.com>
2025-06-01 11:07:48 +0800

    Test Git hooks - demonstrating automatic formatting

---
+++ test-hooks.js
@@ -0,0 +1,9 @@
+// Test file to demonstrate Git hooks
+const test = 'This has bad formatting';
+
+function badSpacing(param) {
+  return param + 'concatenated';
+}
+
+console.log(test);
+console.log(badSpacing('demo'));
