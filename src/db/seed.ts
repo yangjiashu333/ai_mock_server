0bbe64f5e007f5ca6965820e862d6bf3eb71e3d1
yangjiashu.333 <yangjiashu.333@bytedance.com>
2025-06-16 18:20:59 +0800

    seed users

---
+++ src/db/seed.ts
@@ -0,0 +1,25 @@
+import { db } from './index';
+import { usersTable } from './schema';
+
+async function seedUsers() {
+  try {
+    db.insert(usersTable).values([
+      {
+        name: 'admin',
+        age: 20,
+        email: 'admin@example.com',
+      },
+      {
+        name: 'user',
+        age: 20,
+        email: 'user@example.com',
+      },
+    ]);
+    console.log('User table seeded successfully');
+  } catch (error) {
+    console.error('Error seeding user table:', error);
+    process.exit(1);
+  }
+}
+
+seedUsers();
